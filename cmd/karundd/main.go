ccd7f9d221a40676f56c87f05374f2cfc69754ae
Developer Experience team at Tendermint <hello@tendermint.com>
2023-01-01 20:39:27 +0300

    Initialized with Ignite CLI

---
+++ cmd/karundd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/karunnetwork/karund/app"
+	"github.com/karunnetwork/karund/cmd/karundd/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
